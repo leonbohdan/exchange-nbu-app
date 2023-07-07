1bf8b1d08bd3d9f1d43d6695416be048c912b309
Bohdan Leonets <leonetsbogdan@gmail.com>
2023-07-07 20:26:09 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/',
+  base: '/exchange-nbu-app/',
   plugins: [
     vue(),
   ],
